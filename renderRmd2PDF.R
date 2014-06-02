db7b18dd3e96aba45d5fc638c4a6c78451ddd22f
Ciera Martinez <ciera.martinez@gmail.com>
2014-06-02 15:57:24 -0700

    all changes before turing script into skeleton model.

--- renderRmd2PDF.R
+++ renderRmd2PDF.R
@@ -1,8 +1,12 @@
 library(rmarkdown)
-render("tf2cmbr_wtcmbr/tf2cmbr_wtcmbr.Rmd", "pdf_document")
+library(edgeR)
+render("tf2cmbr_wtcmbr/tf2cmbr_wtcmbr.Rmd", "pdf_document") 
 render("tf2ambr_wtaother/wtambr_wtaother.Rmd", "pdf_document")
-render("wtambr_wtbmbr/wtambr_wtbmbr.Rmd", "pdf_document")
-render("wtambr_wtcmbr/wtambr_wtcmbr.Rmd", "pdf_document")
-render("wtbmbr_wtbother.attempt2/wtbmbr_wtbother.attempt2.Rmd", "pdf_document")
+render("wtambr_wtbmbr/wtambr_wtbmbr.Rmd", "pdf_document") 
+render("wtambr_wtcmbr/wtambr_wtcmbr.Rmd", "pdf_document") 
+render("wtbmbr_wtbother/wtbmbr_wtbother.attempt2.Rmd", "pdf_document") 
 render("wtbmbr_wtcmbr/wtbmbr_wtcmbr.Rmd", "pdf_document")
-render("wtcmbr_wtcother/wtcmbr_wtcother.Rmd", "pdf_document")
+render("wtcmbr_wtcother/wtcmbr_wtcother.Rmd", "pdf_document") 
+render("wtaother_wbother/wtaother_wbother.Rmd", "pdf_document")
+
+
